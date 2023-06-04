dfe493b9aae4fa8619ec77323423a7d731fee060
Dharmender <pald4345@gmail.com>
2023-06-04 13:05:22 +0530

    Added Array_Ques

--- Array/Buy_Sell_oneTime.java
+++ Array/Buy_Sell_oneTime.java
@@ -4,16 +4,17 @@ public class Buy_Sell_oneTime {
     public static int maxProfit(int[] prices) {
         int min = prices[0];
         int max = Integer.MIN_VALUE;
-        for(int i = 0; i<prices.length; i++){
-            if(prices[i]<min) min = prices[i];
-            if(max < prices[i]-min){
-                max = prices[i]-min;
+        for (int price : prices) {
+            if (price < min) min = price;
+            if (max < price - min) {
+                max = price - min;
             }
         }
         return max;
     }
 
     public static void main(String[] args) {
+
         System.out.println(maxProfit(new int[]{7,1,5,3,6,4}));
     }
 }
